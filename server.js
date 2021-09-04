0f9d5f1cd9e57b2294efc1ee15d62296cf171364
Matt B <mbaranich@yahoo.com>
2021-09-04 16:56:34 -0400

    Connect and display weather.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== "prodcution") {
 }
 
 const WEATHERSTACK_API_KEY = process.env.WEATHERSTACK_API_KEY;
+const axios = require("axios");
 const express = require("express");
 const app = express();
 
@@ -12,7 +13,11 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.post("/weather", (req, res) => {
-	console.log(req.body);
+	const url = `http://api.weatherstack.com/current?access_key=${WEATHERSTACK_API_KEY}&query=${req.body.latitude},${req.body.longitude}`;
+	axios({
+		url: url,
+		responseType: "json",
+	}).then((data) => res.json(data.data.current));
 });
 
 app.listen(3000, () => {
